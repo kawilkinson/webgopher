c8c1ff715185ed0bf98446635ccee4369a2acb97
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-21 18:44:32 -0400

    add ability for crawler to accept command line arguments

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	arguments := os.Args[1:]
+	if len(arguments) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(arguments) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := arguments[0]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
 }
