cfa3217a7359186efe8c2bc21dcb530fdf007122
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-22 17:37:43 -0400

    add mutex, goroutines, and waitgroups so crawler can crawl more URLs at the same time

---
+++ config.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync"
+)
+
+type config struct {
+	pages              map[string]int
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
+
+func configure(baseURL string, maxConcurrency int) (*config, error) {
+	parsedBaseURL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing provided base URL: %v", err)
+	}
+
+	return &config{
+		pages:              make(map[string]int),
+		baseURL:            parsedBaseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
+	}, nil
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -16,6 +17,19 @@ func main() {
 	}
 
 	baseURL := arguments[0]
+	const maxConcurrency = 5
+	cfg, err := configure(baseURL, maxConcurrency)
+	if err != nil {
+		log.Fatalf("error configuring WebGopher: %v", err)
+	}
+
+	cfg.wg.Add(1)
 	fmt.Printf("starting crawl of %s...\n", baseURL)
-	fmt.Println(getHTML(baseURL))
+	go cfg.crawlPage(baseURL)
+	cfg.wg.Wait()
+
+	fmt.Println("Here are the counts of all the URLs crawled:")
+	for URL, count := range cfg.pages {
+		fmt.Printf("URL: %s\nCount: %d\n\n", URL, count)
+	}
 }
