3e0c878fac0752a688f89c19b593ef2a35aba879
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-22 18:18:56 -0400

    add maxPages to config and allow more arguments

--- config.go
+++ config.go
@@ -7,20 +7,44 @@ import (
 )
 
 type config struct {
+	maxPages           int
 	pages              map[string]int
 	baseURL            *url.URL
 	mu                 *sync.Mutex
 	concurrencyControl chan struct{}
 	wg                 *sync.WaitGroup
+	runOnce            sync.Once
 }
 
-func configure(baseURL string, maxConcurrency int) (*config, error) {
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	_, visited := cfg.pages[normalizedURL]
+	if visited {
+		cfg.pages[normalizedURL] += 1
+		return false
+	}
+
+	cfg.pages[normalizedURL] = 1
+	return true
+}
+
+func (cfg *config) isMaxPagesReached() (maxReached bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	return len(cfg.pages) >= cfg.maxPages
+}
+
+func configure(baseURL string, maxConcurrency, maxPages int) (*config, error) {
 	parsedBaseURL, err := url.Parse(baseURL)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing provided base URL: %v", err)
 	}
 
 	return &config{
+		maxPages:           maxPages,
 		pages:              make(map[string]int),
 		baseURL:            parsedBaseURL,
 		mu:                 &sync.Mutex{},
